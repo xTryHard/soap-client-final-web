90604006fda18f8296229a7598b022e8c28ee4b4
xTryHard <joser210507@gmail.com>
2021-04-25 23:44:19 -0400

    Rafa: create SoapClient class

--- soap.js
+++ soap.js
@@ -1,2 +1,25 @@
 'use strict';
 
+const soap = require('soap');
+
+class SoapClient {
+
+    constructor(url) {
+        this.url = url;
+        this.client = soap.createClientAsync(url);
+    }
+
+
+    getFormsByUsername(username) {
+        this.client.then((clientReference) => {
+            return client.getFormsByUsername({arg0: username});
+        }).then(result => {return result[0]});
+    }
+
+    postForm(form) {
+        this.client.then((clientReference) => {
+            return clientReference.postFormAsync({arg0: form});
+        }).then(result => {return result[0]});
+    }
+}
+
