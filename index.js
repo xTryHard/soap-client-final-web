7c0d5dcd0ff477f2d57851685981e991b1b469ab
xTryHard <joser210507@gmail.com>
2021-05-01 01:33:52 -0400

    Rafa: almost there. Login missing

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ const express = require('express');
 const app = express();
 const morgan = require('morgan');
 const SoapClient = require('./soapClient');
+const fetch = require('node-fetch');
+const cookieParser = require('cookie-parser');
 
 const soapClient = new SoapClient('http://192.168.0.101:7000/ws/FormWebService?wsdl');
 const path = require('path');
@@ -15,9 +17,53 @@ app.use(express.json());
 //     limit: "10mb"
 // }));
 app.use(morgan('dev'));
+app.use(cookieParser());
+
+function validate(req, res, next) {
+    if (req.cookies.user == null) {
+        res.redirect('/login');
+    }
+    console.log(req.cookies);
+    next();
+}
+
+app.use(validate);
 
 app.get('/', (req, res) => {
+    res.cookie('test', 'test', {maxAge: 10800});
+
+
     res.sendFile('/public/html/index.html', {
+        root: __dirname
+    });
+});
+
+app.get('/validate-login', async (req, res) => {
+   const credentials = req.body;
+   let result = await fetch('http://192.168.0.101:7000/api/v1/login/', {
+       method: 'POST',
+       headers: {
+           'Content-Type': 'application/json'
+       },
+       body: JSON.stringify({
+           username: credentials.username,
+           password: credentials.password
+       })
+   });
+
+   result = await result.json();
+   console.log(result);
+
+   if (result.token != null && result.expires_in != null) {
+       sessionStorage.setItem('user', credentials.username);
+       res.redirect('/');
+   } else {
+       res.redirect('/login');
+   }
+});
+
+app.get('/login', (req, res) => {
+    res.sendFile('/public/html/login.html', {
         root: __dirname
     });
 });
